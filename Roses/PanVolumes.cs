67b902c468d1e307a81af1c695f498e8db8bf7d3
RachelBarnes <rachelbarnesjn116@yahoo.com>
2016-05-04 10:59:36 -0400

    First Commit in the Second Take

---
+++ Roses/PanVolumes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roses
+{
+    public class SqaureFN
+    {
+        public Func<int, int> sqaure = x => x * x;
+    }
+    public class Percent
+    {
+        public Func<double, double, int> percent = (x, y) => Convert.ToInt32((x / y) * 100);
+        //need to have these be doubles or floats, otherwise you'll get an int of 2/10, which is 0 * 100, which is 0. 
+    }
+    public class RoundPan : IGetVolume
+    {
+        private int _diameter;
+        private int _depth;
+        public RoundPan(int diameter, int depth)
+        {
+            this._diameter = diameter;
+            this._depth = depth;
+        }
+        int IGetVolume.GetVolume()
+        {
+            var square = new SqaureFN();
+            return Convert.ToInt32(Math.PI * (square.sqaure(_diameter / 2)) * _depth);
+        }
+    }
+    public class SquarePan : IGetVolume
+    {
+        private int _width;
+        private int _depth;
+        public SquarePan(int width, int depth)
+        {
+            this._width = width;
+            this._depth = depth;
+        }
+        int IGetVolume.GetVolume()
+        {
+            return Convert.ToInt32(_width * _width * _depth);
+        }
+    }
+    public class RectangularPan : IGetVolume
+    {
+        private int _width;
+        private int _length;
+        private int _depth;
+        public RectangularPan(int width, int length, int depth)
+        {
+            this._width = width;
+            this._length = length;
+            this._depth = depth;
+        }
+        int IGetVolume.GetVolume()
+        {
+            return Convert.ToInt32(_width * _length * _depth);
+        }
+    }
+    public class Aggregater 
+    {
+        public int GetAggregatedArea(List<IGetVolume> Pans)
+        {
+            var total = 0;
+            return Pans.Aggregate(total, (accumulatingVolume, pan) => accumulatingVolume + pan.GetVolume());
+        }
+    }
+}
+
---
+++ Tests.cs
@@ -0,0 +1,124 @@
+﻿using RachelsRosesPrototype;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RachelsRosesPrototype
+{
+    [TestFixture]
+    class VolumeTests
+    {
+        [Test]
+        public void Round8x2()
+        {
+            var vol = new RoundPanVolume();
+            int diameter = 8;
+            int height = 2;
+            var volume = vol.CalcuateVolumeRoundPan(diameter, height);
+            Assert.AreEqual(101, Convert.ToInt32(volume));
+        }
+        [Test]
+        public void Round12x2()
+        {
+            var vol = new RoundPanVolume();
+            var diameter = 12;
+            int height = 2;
+            var volume = vol.CalcuateVolumeRoundPan(diameter, height);
+            Assert.AreEqual(226, Convert.ToInt32(volume));
+        }
+        [Test]
+        public void Square8x2()
+        {
+            var vol = new SquarePanVolume();
+            var length = 8;
+            var depth = 2;
+            var volume = vol.CalculateVolumeSquareBakingPan(length, depth);
+            Assert.AreEqual(128, Math.Round((double)volume));
+        }
+        [Test]
+        public void Sqaure6x3()
+        {
+            var vol = new SquarePanVolume();
+            var length = 6;
+            var depth = 3;
+            var volume = vol.CalculateVolumeSquareBakingPan(length, depth);
+            Assert.AreEqual(108, Math.Round((double)volume));
+        }
+        [Test]
+        public void Rect13x9()
+        {
+            var vol = new RectangularPanVolume();
+            var length = 13;
+            var width = 9;
+            var depth = 2;
+            var volume = vol.CalculateVolumeRectangularPan(length, width, depth);
+            Assert.AreEqual(234, volume);
+        }
+        [Test]
+        public void Rect8x12()
+        {
+            var vol = new RectangularPanVolume();
+            var length = 12;
+            var width = 8;
+            var depth = 2;
+            var volume = vol.CalculateVolumeRectangularPan(length, width, depth);
+            Assert.AreEqual(192, Math.Round((double)volume));
+        }
+        [Test]
+        public void CentToIn()
+        {
+            var convert = new VolumeMethods();
+            var cent = 25;
+            var centtoin = convert.ConvertCentimetersToInches(cent);
+            Assert.AreEqual(9.84d, Math.Round((double)centtoin));
+        }
+        [Test]
+        public void InToCent()
+        {
+            var convert = new VolumeMethods();
+            var inch = 12;
+            var inchtocent = convert.ConvertCentimetersToInches(inch);
+            Assert.AreEqual(30.48d, Math.Round((double)inchtocent));
+        }
+        [Test]
+        public void Percent()
+        {
+            var percent = new CalculatePercent();
+            var vol1 = 50;
+            var vol2 = 100;
+            Assert.AreEqual(50, Convert.ToInt32(percent.calculatePercent(vol1, vol2)));
+        }
+        [Test]
+        public void Percent2()
+        {
+            var percent = new CalculatePercent();
+            var vol1 = 12;
+            var vol2 = 40;
+            Assert.AreEqual(30, Convert.ToInt32(percent.calculatePercent(vol1, vol2)));
+        }
+        [Test]
+        public void CheckToSeeIfAggregateWorks()
+        {
+            var aggregateVolumes = new Aggregate();
+            var firstVol = 55;
+            var secondVol = 80;
+            var thirdVol = 90;
+            var aggregate = aggregateVolumes.aggregate(firstVol, secondVol, thirdVol);
+            Assert.AreEqual(225, aggregate); 
+        }
+        [Test]
+        public void Aggregate2Tier()
+        {
+            var aggregate = new Aggregate();
+            var volume = new VolumeMethods();
+            var firstVol = volume.RoundPanVolume(10, 2);
+            var secVol = volume.SquarePanVolume(7, 2);
+            var aggregateVolumes = aggregate.aggregate(firstVol, secVol);
+            Assert.AreEqual(255, aggregateVolumes); 
+        }
+
+    }
+}
